ad251c258c5f059cd93bea742d1970fb92e3bc08
Rajesh Korde <rajesh.korde@gmail.com>
2024-07-04 14:51:33 -0700

    removed .aws-sam from commits

--- examgpt-backend/.aws-sam/deps/dd25b4e1-f001-4242-942b-3d6806bcc519/botocore-stubs/docs/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Set
-
-from botocore.session import Session
-
-DEPRECATED_SERVICE_NAMES: Set[str] = ...
-
-def generate_docs(root_dir: str, session: Session) -> None: ...
--- examgpt-backend/.aws-sam/deps/dd25b4e1-f001-4242-942b-3d6806bcc519/botocore-stubs/retries/base.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-class BaseRetryBackoff:
-    def delay_amount(self, context: Any) -> float: ...
-
-class BaseRetryableChecker:
-    def is_retryable(self, context: Any) -> bool: ...
