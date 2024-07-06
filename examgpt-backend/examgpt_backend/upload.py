56ef91c2d7365cd93a62848ba426b45302f42196
Rajesh Korde <rajesh.korde@gmail.com>
2024-07-05 17:42:12 -0700

    fixed uploading of file

--- examgpt-backend/examgpt_backend/upload.py
+++ examgpt-backend/examgpt_backend/upload.py
@@ -4,21 +4,28 @@ import os
 from typing import Any
 
 import boto3
+from botocore.exceptions import ClientError
 
 s3 = boto3.client("s3")
 logger = logging.getLogger(__name__)
 
 
 def create_presigned_url(
-    bucket_name: str, object_name: str, expiration: int = 3600
+    bucket_name: str,
+    object_name: str,
+    fields=None,
+    conditions=None,
+    expiration: int = 3600,
 ) -> str | None:
     try:
-        response = s3.generate_presigned_url(
-            "get_object",
-            Params={"Bucket": bucket_name, "Key": object_name},
+        response = s3.generate_presigned_post(
+            bucket_name,
+            object_name,
+            Fields=fields,
+            Conditions=conditions,
             ExpiresIn=expiration,
         )
-    except Exception as e:
+    except ClientError as e:
         logger.error(e)
         return None
 
@@ -38,12 +45,21 @@ def get_error(message: str = "Something went wrong!") -> dict[str, Any]:
 
 
 def handler(event: dict[Any, Any], context) -> dict[str, Any]:
+    print(f"Context type: {type(context)}")
     bucket_name = os.environ["CONTENT_BUCKET"]
     if not bucket_name:
         logger.error("Could not find bucket name in environment variables")
         return get_error()
 
-    signed_url = create_presigned_url(bucket_name, object_name="test")
+    if "filename" in event["body"]:
+        filename = event["body"]["filename"]
+    else:
+        print("No property called filename in request")
+        print(f"{event=}")
+        filename = "test/test.pdf"
+    print(f"{filename=}")
+
+    signed_url = create_presigned_url(bucket_name, object_name=filename)
     if not signed_url:
         message = "Could not generate pre-signed URL"
         logger.error(message)
--- examgpt-backend/scripts/upload_file.py
+++ examgpt-backend/scripts/upload_file.py
@@ -1,20 +1,42 @@
+from pathlib import Path
 from typing import Any
 
 import requests
 
 api_url = "https://aer2di0h2m.execute-api.us-west-2.amazonaws.com/Stage/upload"
+file_path = "testdata/aws2.pdf"
 
 
-def get_presigned_url(api_url: str, payload: dict[str, Any]) -> str:
-    response = requests.post(api_url, json=payload)
+def get_presigned_url(api_url: str, file_path: str):
+    response = requests.post(api_url, json={"filename": file_path})
     response.raise_for_status()
-    presigned_url = response.json().get("url")
-    return presigned_url
+    presigned_url = response.json()["url"]["url"]
+    fields = response.json()["url"]["fields"]
+    return presigned_url, fields
+
+
+def upload_file_to_s3(
+    presigned_url: str, fields: dict[str, Any], file_path: str
+) -> None:
+    # TODO: perform file size checks
+    with open(file_path, "rb") as file:
+        files = {"file": (file_path, file)}
+        response = requests.post(presigned_url, data=fields, files=files)
+
+    response.raise_for_status()
 
 
 def main() -> None:
-    url = get_presigned_url(api_url, payload={})
+    url, fields = get_presigned_url(api_url, file_path)
     print(f"Presigned Url: {url}")
+    print(f"fields: {fields}")
+    print(f"Fields_type: {type(fields)}")
+
+    if not Path(file_path).exists():
+        print(f"Error: file not found: {file_path}")
+        return
+    upload_file_to_s3(url, fields, file_path=file_path)
+    print("File uploaded successfully")
 
 
 if __name__ == "__main__":
