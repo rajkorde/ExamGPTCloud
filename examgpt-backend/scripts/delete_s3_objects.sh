cb6796d8f24131e69f47afb4d3a6c21f66d703bb
Rajesh Korde <rajesh.korde@gmail.com>
2024-07-29 17:41:25 -0700

    updated script to delete dynamodb items

--- examgpt-backend/scripts/delete_s3_objects.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-bucket_name=$(aws s3 ls | grep "examgpt"| awk '{print $3}')
-echo "Deleting objects from bucket: $bucket_name"
-aws s3 rm s3://$bucket_name --recursive
--- examgpt-backend/scripts/list_ecr_repos.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-aws ecr describe-repositories --query 'repositories[].repositoryName' --output table
