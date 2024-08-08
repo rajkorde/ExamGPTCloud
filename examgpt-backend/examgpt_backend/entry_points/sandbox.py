a4aade6d7bd0caf399f05777b0943edbc4e16414
Rajesh Korde <rajesh.korde@gmail.com>
2024-08-08 15:09:09 -0700

    moved sandbox.py code to entry_points and did local testing to make sure everything works

--- examgpt-backend/examgpt_backend/entry_points/sandbox.py
+++ examgpt-backend/examgpt_backend/entry_points/sandbox.py
@@ -1,10 +1,12 @@
 import json
 from typing import Any
 
+from ai.constants import ModelFamily
+
 
 def handler(event: dict[Any, Any], context: Any) -> dict[str, Any]:
     print("Executing sandbox.")
-
+    print(ModelFamily.GOOGLE.value)
     return {
         "statusCode": 200,
         "body": json.dumps({"value": "test"}),
