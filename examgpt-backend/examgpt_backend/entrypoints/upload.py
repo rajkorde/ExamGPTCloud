4dbd6933a53a5263915c84d3a9452bfcbbdc0056
Rajesh Korde <rajesh.korde@gmail.com>
2024-08-09 17:13:37 -0700

    added a test event for upload.py

--- examgpt-backend/examgpt_backend/entrypoints/upload.py
+++ examgpt-backend/examgpt_backend/entrypoints/upload.py
@@ -5,7 +5,8 @@ from typing import Any, Optional
 
 import boto3
 from botocore.exceptions import ClientError
-from domain.model.core.exam import Exam
+
+# from domain.model.core.exam import Exam
 from domain.model.utils.logging import app_logger
 from entrypoints.helpers.utils import get_env_var, get_error
 from pydantic import ValidationError
@@ -17,49 +18,49 @@ ddb = boto3.resource("dynamodb")
 logger = logging.getLogger(__name__)
 
 
-def create_presigned_url(
-    bucket_name: str,
-    object_name: str,
-    fields: Optional[dict[str, Any]] = None,
-    conditions: Optional[list[dict[str, Any]]] = None,
-    expiration: int = 3600,
-) -> str | None:
-    try:
-        response = s3.generate_presigned_post(
-            bucket_name,
-            object_name,
-            Fields=fields,
-            Conditions=conditions,
-            ExpiresIn=expiration,
-        )
-    except ClientError as e:
-        logger.error(e)
-        return None
+# def create_presigned_url(
+#     bucket_name: str,
+#     object_name: str,
+#     fields: Optional[dict[str, Any]] = None,
+#     conditions: Optional[list[dict[str, Any]]] = None,
+#     expiration: int = 3600,
+# ) -> str | None:
+#     try:
+#         response = s3.generate_presigned_post(
+#             bucket_name,
+#             object_name,
+#             Fields=fields,
+#             Conditions=conditions,
+#             ExpiresIn=expiration,
+#         )
+#     except ClientError as e:
+#         logger.error(e)
+#         return None
 
-    # The response contains the presigned URL
-    return response
-
-
-def get_item(body: dict[str, Any], item: str):
-    if item not in body:
-        print(f"No property called {item} in request")
-        return None
-    return body[item]
+#     # The response contains the presigned URL
+#     return response
 
 
-def parse_event(event: dict[Any, Any]):
-    body_json = json.loads(event["body"])
-    filename = get_item(body_json, "filename")
-    exam_name = get_item(body_json, "exam_name")
-    return filename, exam_name
+# def get_item(body: dict[str, Any], item: str):
+#     if item not in body:
+#         print(f"No property called {item} in request")
+#         return None
+#     return body[item]
 
 
-def save_exam(exam: Exam, table_name: str):
-    table = ddb.Table(table_name)
-    try:
-        table.put_item(Item=exam.model_dump())
-    except ValidationError as e:
-        print(f"Validation error: {e}")
+# def parse_event(event: dict[Any, Any]):
+#     body_json = json.loads(event["body"])
+#     filename = get_item(body_json, "filename")
+#     exam_name = get_item(body_json, "exam_name")
+#     return filename, exam_name
+
+
+# def save_exam(exam: Exam, table_name: str):
+#     table = ddb.Table(table_name)
+#     try:
+#         table.put_item(Item=exam.model_dump())
+#     except ValidationError as e:
+#         print(f"Validation error: {e}")
 
 
 def handler(event: dict[Any, Any], context: Any) -> dict[str, Any]:
