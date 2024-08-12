151ff67c3d4c5882551c0797b25f7b9c4de1fd1c
Rajesh Korde <rajesh.korde@gmail.com>
2024-08-12 13:49:34 -0700

    added ExamState

--- examgpt-backend/examgpt_backend/domain/model/core/exam.py
+++ examgpt-backend/examgpt_backend/domain/model/core/exam.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Optional
 from uuid import uuid4
 
@@ -7,8 +8,16 @@ from domain.model.utils.misc import get_current_time
 from pydantic import BaseModel, Field
 
 
+class ExamState(Enum):
+    CREATED = "created"
+    UPLOADED = "uploaded"
+    CHUNKED = "chunked"
+    QAGENERATED = "qa_generated"
+
+
 class Exam(BaseModel):
     name: str
     sources: list[str] = Field(default_factory=list)
     exam_code: Optional[str] = Field(default_factory=lambda: codenamize(str(uuid4())))
     last_updated: str = Field(default_factory=get_current_time)
+    state: ExamState = Field(default=ExamState.CREATED)
