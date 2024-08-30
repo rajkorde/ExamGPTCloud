6a3ab23f7b5e98ee199ac2d27e54fe59dae52312
Rajesh Korde <rajesh.korde@gmail.com>
2024-08-30 09:20:59 -0700

    added functionality to retrieve open ai key from SSM

--- examgpt-backend/examgpt_backend/entrypoints/api/generate.py
+++ examgpt-backend/examgpt_backend/entrypoints/api/generate.py
@@ -1,9 +1,10 @@
-import os
 from typing import Any
 
 import boto3
 from domain.command_handlers.chunks_commands_handler import get_chunks
+from domain.command_handlers.environments_commands_handler import get_parameter
 from domain.commands.chunks_commands import GetChunks
+from domain.commands.environment_commands import GetParameter
 from domain.model.utils.logging import app_logger
 from entrypoints.helpers.utils import CommandRegistry, get_error, get_success
 from entrypoints.models.api_model import GenerateQARequest
@@ -17,20 +18,6 @@ ddb = boto3.resource("dynamodb")
 logger = app_logger.get_logger()
 
 
-def get_parameter(parameter_name: str, with_decryption: bool = True):
-    try:
-        response = ssm.get_parameter(
-            Name=parameter_name, WithDecryption=with_decryption
-        )
-        return str(response["Parameter"]["Value"])
-    except ssm.exceptions.ParameterNotFound:
-        logger.error(f"The parameter {parameter_name} was not found.")
-        return None
-    except Exception as e:
-        logger.error(f"An error occurred: {str(e)}")
-        return None
-
-
 def handler(event: dict[str, Any], context: Any):
     logger.debug(f"{event=}")
     message = "Generating QAs based on notifcation."
@@ -38,6 +25,7 @@ def handler(event: dict[str, Any], context: Any):
 
     command_registry = CommandRegistry()
     chunk_service = command_registry.get_chunk_service()
+    environment_service = command_registry.get_environment_service()
 
     # parse request (Get all chunks)
     qa_request = GenerateQARequest.parse_event(event)
@@ -59,6 +47,14 @@ def handler(event: dict[str, Any], context: Any):
 
     # Create QA objects for each chunk, if not already created
     ## Get OpenAI key
+    openai_key = get_parameter(
+        GetParameter(name=openai_key_name, is_encrypted=True), environment_service
+    )
+    if not openai_key:
+        logger.error("Error: Could not get OpenAI key")
+        return get_error()
+    logger.debug(f"{openai_key=}")
+
     ## Create OpenAI model
     ## Create QA objects
 
@@ -74,21 +70,21 @@ def handler(event: dict[str, Any], context: Any):
 
     # return 200
 
-    chunk_table = os.environ["CHUNK_TABLE"]
-    if not chunk_table:
-        logger.error("Error: Could not find chunk table in environment variables")
-    logger.debug(f"{chunk_table=}")
+    # chunk_table = os.environ["CHUNK_TABLE"]
+    # if not chunk_table:
+    #     logger.error("Error: Could not find chunk table in environment variables")
+    # logger.debug(f"{chunk_table=}")
 
-    key = get_parameter(parameter_name=openai_key_name)
-    keyname = openai_key_name.split("/")[-1]
-    if not key:
-        logger.error(f"Error: Incorrect key from ssm parameter store: {key}")
-    else:
-        os.environ[keyname] = key
-    logger.debug(f"{key=}")
+    # key = get_parameter(parameter_name=openai_key_name)
+    # keyname = openai_key_name.split("/")[-1]
+    # if not key:
+    #     logger.error(f"Error: Incorrect key from ssm parameter store: {key}")
+    # else:
+    #     os.environ[keyname] = key
+    # logger.debug(f"{key=}")
 
-    chunk = event["Records"][0]["Sns"]["Message"]
-    logger.debug(f"{chunk=}")
+    # chunk = event["Records"][0]["Sns"]["Message"]
+    # logger.debug(f"{chunk=}")
 
     # model = OpenAIProvider()
     # chat = model.get_chat_model()
