1d4de43278d87200d15da03149d0c2387662ac88
Rajesh Korde <rajesh.korde@gmail.com>
2024-09-09 17:57:49 -0700

    added code to extract exam object, given an exam id

--- examgpt-backend/examgpt_backend/entrypoints/api/validate.py
+++ examgpt-backend/examgpt_backend/entrypoints/api/validate.py
@@ -1,6 +1,7 @@
-import json
 from typing import Any
 
+from domain.command_handlers.exam_commands_handler import get_exam
+from domain.commands.exam_commands import GetExam
 from domain.model.utils.logging import app_logger
 from entrypoints.helpers.utils import CommandRegistry, get_error, get_success
 from entrypoints.models.api_model import ValidateRequest
@@ -12,14 +13,18 @@ def handler(event: dict[Any, Any], context: Any) -> dict[str, Any]:
     logger.info("Executing validator.")
     logger.info(f"{event=}")
 
-    exam = ValidateRequest.parse_event(event)
-    if not exam:
+    command_registry = CommandRegistry()
+    exam_service = command_registry.get_exam_service()
+
+    exam_event = ValidateRequest.parse_event(event)
+    if not exam_event:
         logger.error("Error: Could not parse event")
         return get_error()
 
-    exam_code = exam.exam_code
+    exam_code = exam_event.exam_code
 
     # Get exam and validate state is at least chunked and not ready
+    exam = get_exam(GetExam(exam_code=exam_code), exam_service)
 
     # Get chunks
 
